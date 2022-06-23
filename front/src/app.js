9eb5bbd9579271dd7804891735bc3a3cf87da105
kiasarm <kiasarm@automarketing.co.kr>
2022-06-23 16:31:12 +0900

    add Router

---
+++ front/src/app.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Routes, useRoutes } from "react-router-dom";
+import routeData from "./router";
+
+const Router = () => useRoutes(routeData);
+
+const App = () => {
+  return (
+    <Routes>
+      <Router />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ front/src/router.js
@@ -0,0 +1,5 @@
+import Home from "./page/home";
+
+const Router = [{ path: "/", element: <Home /> }];
+
+export default Router;
